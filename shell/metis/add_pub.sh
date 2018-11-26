a044a59757ee2262fd2c31a114bf4f4691789467
zj <m13528404930@163.com>
2018-11-26 10:05:14 +0800

    add metis shell

--- shell/metis/add_pub.sh
+++ shell/metis/add_pub.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type:application/json" -d  '{"name":"meipai_live_msg","topic":"meipai_live","brokers":["172.31.52.157:9092"],"zklist":["172.31.52.157:2181"]}'  "http://0.0.0.0:8860/pubs"
+curl -H "Content-Type:application/json" -d  '{"name":"zj9_test1_pub","topic":"zchtest20171027","brokers":["192.168.199.11:9092"],"zklist":["192.168.199.11:2181"]}'  "http://0.0.0.0:8860/pubs"
