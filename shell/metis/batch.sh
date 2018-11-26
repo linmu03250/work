b5f0a962f7ae740d7ba8b45ce37d80278b8d760a
zj <m13528404930@163.com>
2018-11-26 10:05:59 +0800

    add metis shell

---
+++ shell/metis/batch.sh
@@ -0,0 +1,5 @@
+for ((i=1; i<=100; i ++))
+do
+  sh disable_filter.sh
+  sh enable_filter.sh
+done
---
+++ shell/metis/disable_filter.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type:application/json" -d ''  "http://0.0.0.0:8860/filters/disable/filter1_meilai_live_msg"
