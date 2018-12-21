0ee6b8d4fe856e8ea2ceaac3a77b1cacc6dec65a
zj <m13528404930@163.com>
2018-12-21 16:51:40 +0800

    增加cmake

---
+++ cmake/cmake_DEMO/README.md
@@ -0,0 +1,5 @@
+CMake-Demo
+=====
+git clone https://github.com/wzpan/cmake-demo.git
+
+博客链接： http://www.hahack.com/codes/cmake/
--- shell/metis/add_filter.sh
+++ shell/metis/add_filter.sh
@@ -1,3 +1,1 @@
 curl -H "Content-Type:application/json" -d '{"name":"zj9_test1_filter","pubname":["zj9_test1_pub"],"program":".","topic":"zj_test"}'  "http://0.0.0.0:8860/filters"
-
-#curl -H "Content-Type:application/json" -d '{"name":"filter1_meilai_live_msg","pubname":"meipai_live_msg","program":".","topic":"filter_meipai_live_msg_1"}'  "http://0.0.0.0:8860/filters"
